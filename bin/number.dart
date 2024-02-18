607b4dd4a24e39a40507459cefa9a5e5e994d585
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-02-18 21:25:53 +0600

    number data type

---
+++ bin/number.dart
@@ -0,0 +1,9 @@
+main() {
+  int number1 = 2;
+  int number2 = 4;
+  print(number1 + number2);  // integer number
+
+  double number3 = 2.4;
+  double number4 = 3.7;
+  print(number3 + number4);  // double number
+}
