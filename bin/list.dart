92297add0e9cb74032ebb2d84937e413d097bd7f
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-02-19 15:47:04 +0600

    list property added

--- bin/list.dart
+++ bin/list.dart
@@ -1,4 +1,14 @@
 main() {
-  List<String> studentName = ['Sunny', 'Shakil', 'Rafi', 'Siam' ];
-  print(studentName);
+  List<String> studentName = ['Sunny', 'Shakil', 'Rafi', 'Siam'];
+  print(studentName);                   // [Sunny, Shakil, Rafi, Siam]
+
+  // List Properties
+  print(studentName.first);             //  Sunny
+  print(studentName.last);              //  Siam
+  print(studentName.length);            //  4
+  print(studentName.reversed);          //  (Siam, Rafi, Shakil, Sunny)
+  print(studentName.isEmpty);           //  false
+  print(studentName.isNotEmpty);        //  true
+  print(studentName.runtimeType);       //  List<String>
+  print(studentName.indexed);           // ((0, Sunny), (1, Shakil), (2, Rafi), (3, Siam))
 }
