655c5d3fd92e5dd7091b68ac5ae15a81b1d569aa
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-02-20 13:43:09 +0600

    const and final keyword added

---
+++ bin/constants.dart
@@ -0,0 +1,10 @@
+main() {
+
+  // Compile time constant
+  const String myName = 'Sunny';
+  print(myName);                // Sunny
+
+  // Run time constant
+  final int rollNo = 18;
+  print(rollNo);                // 18
+}
