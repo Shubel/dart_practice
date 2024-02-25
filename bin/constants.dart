daa51d771e450ee64814fac3d351cfdab0005ed2
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-02-25 13:49:47 +0600

    Compile time constant(const) and Run time constant(final) keyword added

--- bin/constants.dart
+++ bin/constants.dart
@@ -1,10 +1,10 @@
 main() {
 
-  // Compile time constant
+  // Compile time constant "কম্পাইল-টাইমে গণনাযোগ্য অপরিবর্তনশীল টাইপ ও অপরিবর্তনশীল ভ্যালু"
   const String myName = 'Sunny';
   print(myName);                // Sunny
 
-  // Run time constant
+  // Run time constant  "রান-টাইমে গণনাযোগ্য অপরিবর্তনশীল টাইপ ও অপরিবর্তনশীল ভ্যালু"
   final int rollNo = 18;
   print(rollNo);                // 18
 }
