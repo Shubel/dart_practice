a3d33cbfcfd700da406c586d1fc0682355f174b0
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-02-26 16:56:07 +0600

    basic function added

---
+++ bin/function.dart
@@ -0,0 +1,9 @@
+main() {
+  wishesMessage('Sunny', 'A+');
+  wishesMessage('Shakil', 'B+');
+}
+
+wishesMessage(String name, String result) {
+  print('Good morning, $name');
+  print('You got $result');
+}
