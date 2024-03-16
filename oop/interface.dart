23147e01d29b5dcb5b47fcfa33f6fb9982e013fc
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-03-16 14:57:03 +0600

    interface and implementation in dart

---
+++ oop/interface.dart
@@ -0,0 +1,46 @@
+import 'human.dart';
+
+class Shakil extends Human {
+  // interface
+  @override
+  void eating() {
+    _movingTheHands();
+  }
+
+  // implementation
+  void _movingTheHands() {
+    print('Moving');
+  }
+
+  @override
+  void moving() {
+    // TODO: implement moving
+  }
+
+  @override
+  void dummy() {
+    // TODO: implement dummy
+  }
+}
+
+class Hasan implements Human {
+  @override
+  void eating() {
+    // TODO: implement eating
+  }
+
+  @override
+  void moving() {
+    // TODO: implement moving
+  }
+
+  @override
+  void talking() {}
+
+  @override
+  void dummy() {
+    // TODO: implement dummy
+  }
+}
+
+abstract class Jakir implements Human {}
