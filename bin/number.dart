96038eaab07a2afcdcb809069cef42eb265efcc2
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-05-21 10:08:54 +0600

    dart number file added

--- bin/number.dart
+++ bin/number.dart
@@ -6,4 +6,10 @@ main() {
   double number3 = 2.4;
   double number4 = 3.7;
   print(number3 + number4);  // double number
+
+  num num5 = 1;
+  num num6 = 2.052;
+  num result = num5 + num6;
+  print(result);                // 3.052
+  print(result.runtimeType);    // double
 }
